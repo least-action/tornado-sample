d0e390bf78a63ffd8d84f89f45f583ff36b8e94d
least-action <taewoo.kim.f.r@gmail.com>
2020-09-20 01:08:34 +0900

    set logger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tornado.ioloop
 import tornado.web
 import logging
+import time
 import json
 
 from handlers.base_handlers import HomeHandler, PingHandler
@@ -19,11 +20,20 @@ if __name__ == "__main__":
     with open('config.json', 'r') as f:
         config = json.load(f)
 
+    logger = logging.getLogger()
+    logging.addLevelName(1, 'TRACE')
+    logging.trace = lambda msg: logging.log(1, msg)
+    level = getattr(logging, config['logging']['level'].upper())
+    logging.Formatter.converter = time.gmtime
+    logging.basicConfig(format='[%(asctime)s] %(levelname)-5s: %(message)s',
+                        datefmt='%Y-%m-%dT%H:%M:%SZ',
+                        level=level)
+
     try:
         app = make_app()
         app.listen(config['port'])
+        logging.info('server started with port {port}'.format(port=config['port']))
         tornado.ioloop.IOLoop.current().start()
-        logging.info('server started - listening {port} port'.format(port=config['port']))
     except KeyboardInterrupt:
         pass
     except Exception as e:
